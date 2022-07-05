1829172f2fa0fb269287b95bc020ec7c3ec8458d
adel0585 <adel.serub.05@gmail.com>
2022-07-05 22:51:14 +0100

    test

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @click="testFn">
+    hello
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    }),
+
+    methods: {
+      testFn() {
+        alert("ll")
+      }
+    },
+  }
+</script>
